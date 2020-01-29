910f9a872f94b2212e7d239b8c43608cbef48846
casey-tai <ceasedinsanity.ct@gmail.com>
2020-01-29 15:23:27 -0400

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let express = require('express')
+let cors = require('cors')
 let socketIO = require('socket.io')
 
 let app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static(__dirname + "/client"))
 
